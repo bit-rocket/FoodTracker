d1d76e568ca998a89af908982d2ef51771688bd6
pingtouge <ye515430@163.com>
2018-10-10 00:58:54 +0800

    try persist data

--- FoodTracker/Meal.swift
+++ FoodTracker/Meal.swift
@@ -7,12 +7,27 @@
 //
 
 import UIKit
+import os.log
 
-class Meal {
+class Meal: NSObject, NSCoding {
+
+    
     // Mark: Properties
     var name: String
     var photo: UIImage
     var rating: Int
+    
+    // Mark: Archiving Paths
+    static let DocumentDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+    static let ArchiveUrl = DocumentDirectory.appendingPathComponent("meals")
+    
+    // Mark: Types
+    struct Property {
+        static let name = "name"
+        static let photo = "photo"
+        static let rating = "rating"
+    }
+
     
     // Mark: Initializations
     init?(name: String, photo: UIImage?, rating: Int) {
@@ -32,4 +47,23 @@ class Meal {
         }
         self.rating = rating
     }
+    
+    // MARK: NSCoding
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: Property.name)
+        aCoder.encode(photo, forKey: Property.photo)
+        aCoder.encode(rating, forKey: Property.rating)
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        guard let name = aDecoder.decodeObject(forKey: Property.name) as? String else {
+            os_log("Unable to decode the name for a  meal obj.", log: OSLog.default, type: .debug)
+            return nil
+        }
+        let photo = aDecoder.decodeObject(forKey: Property.photo) as? UIImage
+        let rating = aDecoder.decodeObject(forKey: Property.rating)
+        
+        self.init(name: name, photo: photo, rating: rating as! Int)
+    }
+
 }
